0946fcfaa393f3b9c25a74babc2081151f226edc
Will <will@platocreative.co.nz>
2015-07-21 15:35:45 +1200

    adding account number for users

--- code/decorators/AccountPaymentCheckoutPage.php
+++ code/decorators/AccountPaymentCheckoutPage.php
@@ -2,21 +2,22 @@
 /*
 *	AccountPaymentCheckoutPage extends CheckoutPage
 */
-class AccountPaymentCheckoutPage extends DataExtension {	
+class AccountPaymentCheckoutPage extends DataExtension {
 	public function onAfterInit(){
-		Requirements::javascript('swipestripe-account-payments/js/accountpayment.js');	
+		Requirements::javascript('swipestripe-account-payments/js/accountpayment.js');
 	}
-	
+
 	private static $allowed_actions = array (
 		'AccountPaymentFields'
 	);
-	
+
 	public function AccountPaymentFields($request){
 		if(Director::is_ajax()){
+			$customer = Member::currentUser();
 			$fields = CompositeField::create(
-				TextField::create('AccountPaymentNumber', 'Account Number', '')
+				TextField::create('AccountPaymentNumber', 'Account Number', ($customer ? $customer->AccountNumber : ''))
 			)->setName('AccountPaymentFields');
-			
+
 			return $fields->FieldHolder();
 		}
 		return false;
---
+++ code/decorators/AccountPaymentCustomer.php
@@ -0,0 +1,48 @@
+<?php
+/*
+*	AccountPaymentCustomer extends Customer
+*/
+class AccountPaymentCustomer extends DataExtension {
+	public static $db = array(
+		'AccountNumber' => 'Varchar(250)'
+	);
+
+	public static $has_one = array(
+	);
+
+	public static $has_many = array(
+	);
+
+	public function updateCMSFields(FieldList $fields){
+		return $fields;
+	}
+}
+
+/*
+*	AccountPaymentAccountPage_Controller extends AccountPage_Controller
+*/
+class AccountPaymentAccountPage_Controller extends DataExtension {
+	// Registration form
+	public function updateRegisterAccountFields($fields, $customer, $required){
+		$fields->insertBefore(TextField::create('AccountNumber', 'Account Number'), 'Password');
+		return $fields;
+	}
+
+	// Do account registration
+	public function updateAccountRegister($data, $customer){
+		$customer->AccountNumber = isset($data['AccountNumber']) ? $data['AccountNumber'] : '';
+		return $customer;
+	}
+
+	// Edit form
+	public function updateEditAccountFields($fields, $customer, $required){
+		$fields->insertBefore(TextField::create('AccountNumber', 'Account Number', $customer->AccountNumber), 'Password');
+		return $fields;
+	}
+
+	// Do edit account info
+	public function updateAccountEdit($data, $customer){
+		$customer->AccountNumber = isset($data['AccountNumber']) ? $data['AccountNumber'] : '';
+		return $customer;
+	}
+}
